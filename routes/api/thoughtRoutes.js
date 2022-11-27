e1967bb097dc4f3fb8c464bc132fbe91d57df9f6
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-11-27 03:43:50 -0600

    added thought route through const

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
