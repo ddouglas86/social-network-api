5b84d51194439ad93f3bdcb93cc3fea6f7f23abf
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-11-27 03:50:35 -0600

    completed thought route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,3 +10,19 @@ const {
     deleteReaction
 } = require('../../controllers/thoughtController');
 
+router.route('/')
+.get(getAllThoughts)
+.post(createThought);
+
+router.route('/:thoughtId')
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+.post(addReaction);
+
+router.route('/:thoughtId/reactions/reactionId')
+.delete(deleteReaction);
+
+module.exports = router;
